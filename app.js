b440b7d0b41ef91180df949c04fda39fcef58e61
YHulko <opfr95@gmail.com>
2023-07-05 16:01:55 +0300

    js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-let productsCountsEl = document.getElementById("recycle");
-let addToCartBtns = document.querySelector(".btn-add-to-cart");
-console.log(productsCountsEl);
+let productsCountEl = document.getElementById("products-count");
+let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
+console.log(productsCountEl);
 console.log(addToCartBtns);
 
 addToCartBtns.forEach((btn) =>
   btn.addEventListener("click", function () {
-    productsCountsEl.textContent = +productsCountsEl.textContent + 1;
+    productsCountEl.textContent = +productsCountEl.textContent + 1;
   })
 );
+
+
+let statusLike = document.querySelectorAll(".like");
+console.log(statusLike);/*-знаходить 6 селекторів-*/
+
+statusLike.forEach((img) => { img.addEventListener("click", 
+function () {let imgSrc = img.src; if (imgSrc === "images/like-white.png") {img.src = "images/like-blue.png";} else { img.src = "images/like-white.png";}}
+
+)});
+  
+   
